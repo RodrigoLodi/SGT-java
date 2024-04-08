cd7477c39f9c5e022ccccce7701a41993517e140
Rodrigo Lodi <rodrigolodi1101@gmail.com>
2024-04-07 21:14:18 -0400

    nova tela de login

--- src/main/java/br/unigran/screen/Login.java
+++ src/main/java/br/unigran/screen/Login.java
@@ -1,97 +1,73 @@
 package br.unigran.screen;
 
-import br.unigran.model.Users;
-import br.unigran.persistencia.Dados;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.util.Arrays;
-import java.util.List;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Login extends JFrame {
-    private JPanel painel1;
-   
-    private JButton logar;
-    private JButton cadastrar;
-    private JTextField login;
+
     private JLabel loginLabel;
-    private JPasswordField senha;
+    private JTextField loginField;
     private JLabel senhaLabel;
-    private JTable tabela = new JTable(10, 2);
-    private JScrollPane scroll;
-    private List teste;
-    
+    private JPasswordField senhaField;
+    private JButton logarButton;
+    private JButton cadastrarButton;
+
     public Login() {
-        alocaComponentes();
-        acoes();
-        configuracoes();
+        criarComponentes();
+        configurarJanela();
     }
 
-    private void alocaComponentes() {
-        setLayout(new BorderLayout());
-        painel1 = new JPanel(new FlowLayout());
-        painel1.setLayout(new BoxLayout(painel1, BoxLayout.Y_AXIS));
-        
-        login = new JTextField(150);
-        loginLabel = new JLabel("Email");
-        
-        senha = new JPasswordField(20);
-        senhaLabel = new JLabel("Senha");
-        
-        painel1.add(loginLabel);
-        painel1.add(login);
-        
-        painel1.add(senhaLabel);
-        painel1.add(senha);
-        
-        logar = new JButton("Logar");
-        cadastrar = new JButton("Cadastrar");
-        
-        painel1.add(logar);
-        painel1.add(cadastrar);
-        
-        add(painel1,BorderLayout.PAGE_START);
-        
-    }
-    private void acoes() {
-        logar.addActionListener((ActionEvent e) -> {
-            if (!login.getText().isEmpty() || senha.getPassword().length == 0) {
-                Dados dados = new Dados();
-                List<Users> users = dados.listarUsers("mail='" + login.getText() + "'");
-                if (users.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Usuario ou senha invalidos!", "Aviso", JOptionPane.WARNING_MESSAGE);
-                } else {
-                    for (Users user : users) {
-                        if (Arrays.equals(user.getPassword(), senha.getPassword())) {
-                            Menu menu = new Menu();
-                            menu.setVisible(true);
-                            dispose();
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Usuario ou senha invalidos!", "Aviso", JOptionPane.WARNING_MESSAGE);
-                        }
-                    }
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "Preencha o login e a senha!", "Aviso", JOptionPane.WARNING_MESSAGE);
+    private void criarComponentes() {
+        loginLabel = new JLabel("Email:");
+        loginField = new JTextField(20);
+        senhaLabel = new JLabel("Senha:");
+        senhaField = new JPasswordField(20);
+        logarButton = new JButton("Logar");
+        cadastrarButton = new JButton("Cadastrar");
+
+        logarButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
             }
         });
-        cadastrar.addActionListener((ActionEvent e) -> {
-           Register register = new Register();
-           register.setVisible(true);
-       });
+
+        cadastrarButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
     }
-    private void configuracoes() {
+
+    private void configurarJanela() {
+        JPanel painel = new JPanel(new GridLayout(4, 4));
+        painel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        painel.setBackground(Color.LIGHT_GRAY);
+
+        JPanel botoesPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        botoesPanel.add(logarButton);
+        botoesPanel.add(cadastrarButton);
+        botoesPanel.setBackground(Color.LIGHT_GRAY);
+
+        painel.add(loginLabel);
+        painel.add(loginField);
+        painel.add(senhaLabel);
+        painel.add(senhaField);
+
+        add(painel, BorderLayout.CENTER);
+        add(botoesPanel, BorderLayout.SOUTH);
+
         setTitle("Login");
-        setSize(600, 400);
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
     }
 }
