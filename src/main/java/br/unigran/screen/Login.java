21a0e2bc7c8d45dac6ae9475fffb22491c6f4548
KelvynWolff <kelvynwolff@gmail.com>
2024-04-08 21:25:56 -0400

    Nova tela de login

--- src/main/java/br/unigran/screen/Login.java
+++ src/main/java/br/unigran/screen/Login.java
@@ -2,24 +2,14 @@ package br.unigran.screen;
 
 import br.unigran.model.Users;
 import br.unigran.persistencia.Dados;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
+import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
-    private JPanel painel1;
+    private JPanel panel;
 
     private JButton logar;
     private JButton cadastrar;
@@ -27,9 +17,8 @@ public class Login extends JFrame {
     private JLabel loginLabel;
     private JPasswordField senha;
     private JLabel senhaLabel;
-    private JTable tabela = new JTable(10, 2);
-    private JScrollPane scroll;
-    private List teste;
+
+
     
     public Login() {
         alocaComponentes();
@@ -38,31 +27,35 @@ public class Login extends JFrame {
     }
 
     private void alocaComponentes() {
-        setLayout(new BorderLayout());
-        painel1 = new JPanel(new FlowLayout());
-        painel1.setLayout(new BoxLayout(painel1, BoxLayout.Y_AXIS));
-
-        login = new JTextField(150);
-        loginLabel = new JLabel("Email");
-
+        panel = new JPanel(new BorderLayout());
+        JPanel painelCentro = new JPanel(new GridLayout(2, 2));
+        JPanel painelBotoes = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panel.setBackground(Color.LIGHT_GRAY);
+        
+        loginLabel = new JLabel("Email:");
+        login = new JTextField(20);
+        senhaLabel = new JLabel("Senha:");
         senha = new JPasswordField(20);
-        senhaLabel = new JLabel("Senha");
-
-        painel1.add(loginLabel);
-        painel1.add(login);
-
-        painel1.add(senhaLabel);
-        painel1.add(senha);
-
+        
         logar = new JButton("Logar");
         cadastrar = new JButton("Cadastrar");
 
-        painel1.add(logar);
-        painel1.add(cadastrar);
+        painelCentro.add(loginLabel);
+        painelCentro.add(login);
+        painelCentro.add(senhaLabel);
+        painelCentro.add(senha);
+        
+        painelBotoes.add(logar);
+        painelBotoes.add(cadastrar);
 
-        add(painel1,BorderLayout.PAGE_START);
+        panel.add(painelCentro, BorderLayout.CENTER);
+        panel.add(painelBotoes, BorderLayout.SOUTH);
+        painelCentro.setBackground(Color.LIGHT_GRAY);
+        painelBotoes.setBackground(Color.LIGHT_GRAY);
 
+        add(panel);
     }
+    
     private void acoes() {
         logar.addActionListener((ActionEvent e) -> {
             if (!login.getText().isEmpty() || senha.getPassword().length == 0) {
@@ -85,13 +78,24 @@ public class Login extends JFrame {
                 JOptionPane.showMessageDialog(null, "Preencha o login e a senha!", "Aviso", JOptionPane.WARNING_MESSAGE);
             }
         });
+        
         cadastrar.addActionListener((ActionEvent e) -> {
-           Register register = new Register();
-           register.setVisible(true);
-       });
+            Register register = new Register();
+            register.setVisible(true);
+        });
     }
+    
     private void configuracoes() {
         setTitle("Login");
-        setSize(600, 400);
+        setSize(300, 130);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Login().setVisible(true);
+        });
     }
 }
