beb6e7fe2fc5226a2e75a9be440e51e80446c992
KelvynWolff <kelvynwolff@gmail.com>
2024-04-08 21:35:28 -0400

    Tela de Registro de usuario

--- src/main/java/br/unigran/screen/Register.java
+++ src/main/java/br/unigran/screen/Register.java
@@ -2,106 +2,92 @@ package br.unigran.screen;
 
 import br.unigran.model.Users;
 import br.unigran.persistencia.Dados;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.Arrays;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 
 public class Register extends JFrame {
-    private JPanel painel1;
-    private JButton botaoSalvar;
-    private JButton botaoCancelar;
-    private JTextField campoEmail;
-    private JTextField campoNome;
-    private JPasswordField campoSenha;
-    private JPasswordField campoConfirmaSenha;
-    private JLabel labelEmail;
-    private JLabel labelNome;
-    private JLabel labelSenha;
-    private JLabel labelConfirmaSenha;
+    private JTextField fieldEmail;
+    private JTextField fieldName;
+    private JPasswordField fieldPassword;
+    private JPasswordField fieldConfirmPassword;
+    private JButton btnSave; // Movido para o escopo da classe
+    private JButton btnCancel; // Movido para o escopo da classe
 
     public Register() {
-        alocaComponentes();
-        acoes();
-        configuracoes();
+        allocateComponents();
+        actions();
+        settings();
     }
 
-    private void alocaComponentes() {
-        setLayout(new BorderLayout());
-        
-        painel1 = new JPanel(new FlowLayout());
-        painel1.setLayout(new BoxLayout(painel1, BoxLayout.Y_AXIS));
-        
-        botaoSalvar = new JButton("Salvar");
-        botaoCancelar = new JButton("Cancelar");
-        
-        campoEmail = new JTextField(50);
-        campoNome = new JTextField(50);
-        campoSenha = new JPasswordField(20);
-        campoConfirmaSenha = new JPasswordField(20);
-        
-        labelEmail = new JLabel("Email");
-        labelNome = new JLabel("Nome");
-        labelSenha = new JLabel("Senha");
-        labelConfirmaSenha = new JLabel("Confirmar Senha");
-        
-        painel1.add(labelEmail);
-        painel1.add(campoEmail);
-        painel1.add(labelNome);
-        painel1.add(campoNome);
-        painel1.add(labelSenha);
-        painel1.add(campoSenha);
-        painel1.add(labelConfirmaSenha);
-        painel1.add(campoConfirmaSenha);
-        painel1.add(botaoSalvar);
-        painel1.add(botaoCancelar);
-        
-        add(painel1,BorderLayout.PAGE_START);
+    private void allocateComponents() {
+        JPanel panel = new JPanel(new BorderLayout(5, 5));
+        panel.setBackground(Color.LIGHT_GRAY);
+
+        JPanel inputPanel = new JPanel(new GridLayout(4, 2, 5, 5));
+        inputPanel.setBackground(Color.LIGHT_GRAY);
+
+        inputPanel.add(new JLabel("Email"));
+        fieldEmail = new JTextField();
+        inputPanel.add(fieldEmail);
+
+        inputPanel.add(new JLabel("Nome"));
+        fieldName = new JTextField();
+        inputPanel.add(fieldName);
+
+        inputPanel.add(new JLabel("Senha"));
+        fieldPassword = new JPasswordField();
+        inputPanel.add(fieldPassword);
+
+        inputPanel.add(new JLabel("Confirmar Senha"));
+        fieldConfirmPassword = new JPasswordField();
+        inputPanel.add(fieldConfirmPassword);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        btnSave = new JButton("Salvar");
+        btnCancel = new JButton("Cancelar");
+        buttonPanel.setBackground(Color.LIGHT_GRAY);
+        buttonPanel.add(btnSave);
+        buttonPanel.add(btnCancel);
+
+        panel.add(inputPanel, BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+
+        add(panel);
     }
 
-    private void acoes() {
-        botaoCancelar.addActionListener((ActionEvent e) -> {
-           this.setVisible(false);
-        });
-        botaoSalvar.addActionListener((ActionEvent e) -> {
-            if (!campoNome.getText().isEmpty()) {
-                if (!campoEmail.getText().isEmpty()) {
-                    char[] senha = campoSenha.getPassword();
-                    char[] confirmaSenha = campoConfirmaSenha.getPassword();
+    private void actions() {
+        btnCancel.addActionListener(e -> setVisible(false));
 
-                    if (senha.length == 0 || !Arrays.equals(senha, confirmaSenha)) {
-                        JOptionPane.showMessageDialog(null, "Senha inválida ou não correspondente", "Aviso", JOptionPane.WARNING_MESSAGE);
-                    } else {
-                        Users user = new Users();
-                        user.setMail(campoEmail.getText());
-                        user.setName(campoNome.getText());
-                        user.setPassword(campoSenha.getPassword());
-                        Dados dados = new Dados();
-                        dados.salvar(user);
-                        JOptionPane.showMessageDialog(null, "Usuario registrado com sucesso!", "Aviso", JOptionPane.WARNING_MESSAGE);
-                        dispose();
-                    }
-                 } else {
-                    JOptionPane.showMessageDialog(null, "Email inválido!", "Aviso", JOptionPane.WARNING_MESSAGE);
-                 }
+        btnSave.addActionListener(e -> {
+            String name = fieldName.getText();
+            String email = fieldEmail.getText();
+            char[] password = fieldPassword.getPassword();
+            char[] confirmPassword = fieldConfirmPassword.getPassword();
+
+            if (name.isEmpty() || email.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Nome ou Email inválido!", "Aviso", JOptionPane.WARNING_MESSAGE);
+            } else if (!Arrays.equals(password, confirmPassword)) {
+                JOptionPane.showMessageDialog(null, "Senha inválida ou não correspondente", "Aviso", JOptionPane.WARNING_MESSAGE);
             } else {
-                JOptionPane.showMessageDialog(null, "Nome inválido!", "Aviso", JOptionPane.WARNING_MESSAGE);
+                Users user = new Users();
+                user.setMail(email);
+                user.setName(name);
+                user.setPassword(password);
+                Dados dados = new Dados();
+                dados.salvar(user);
+                JOptionPane.showMessageDialog(null, "Usuário registrado com sucesso!", "Aviso", JOptionPane.WARNING_MESSAGE);
+                dispose();
             }
         });
     }
 
-    private void configuracoes() {
+    private void settings() {
         setTitle("Register");
-        setSize(400, 300);
+        setSize(400, 200);
+        setLocationRelativeTo(null);
+        setResizable(false);
     }
-    
-    
 }
